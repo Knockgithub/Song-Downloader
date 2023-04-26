1e1dade3fe6799e8747f5e7f008eaab18a72a4f5
Knockgithub <131382142+Knockgithub@users.noreply.github.com>
2023-04-26 21:57:49 +0530

    Update config.py
    Signed-off-by: Knockgithub <131382142+Knockgithub@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    DATABASE = os.environ.get("mongodb+srv://knockinfoin:knock123@cluster0.hovklod.mongodb.net/?retryWrites=true&w=majority")
-    OWNER_ID = set(int(x) for x in os.environ.get("6281117072", "1961162381").split())
+    DATABASE = os.environ.get("DB_URI")
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
     SUPPORT = os.environ.get("SUPPORT")
